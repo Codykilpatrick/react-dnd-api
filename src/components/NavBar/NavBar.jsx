3e4bed18c40b1cc7beb31a914d796deecf6a2b97
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-02 15:58:39 -0500

    update logo for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,16 +5,16 @@ const NavBar = () => {
     <header>
       <img 
         src="/images/logo.svg" 
-        style={{ width: "100px", height: "100px" }}
+        style={{ width: "90px", height: "100px" }}
         className="App-logo"
         alt="logo"
       />
-      <container id="nav-container">
+      <div id="nav-container">
         <Link to="/shop">Shop</Link>
         <Link to="/monster-list">Scary Monsters</Link>
         <Link to="/spell-search">Search for Spells</Link>
         <Link to="/class-list">Class List</Link>
-      </container>
+      </div>
     </header>
   )
 }
